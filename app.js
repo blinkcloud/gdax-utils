2490b819cf2b5043550ab65346c2e2381145548b
Jon Luzader <blinkcloudinfo@gmail.com>
2017-12-03 19:33:53 -0500

    added a 'latest'

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 //copryright 2017 blink.cloud, LLC. 
 //Author: Jonathan S. Luzader
 
+//modules
+var express         = require('express');
+var app             = express();
+var cookieParser    = require('cookie-parser');
+var Gdax            = require('gdax');
+var publicClient    = new Gdax.PublicClient();
 
-var express = require('express');
-var app = express();
-var cookieParser = require('cookie-parser');
-
-var Gdax = require('gdax');
-var publicClient = new Gdax.PublicClient();
+//custom
+var valueAverage    = require('./lib/getValueAverage.js');
+var valueLatest     = require('./lib/getValueLatest.js');
 
 //setup middleware
 app.use(cookieParser());
@@ -22,47 +25,79 @@ app.get('/', function ( req, res ) {
 
 app.get('/utils/products', function ( req, res ) {
     publicClient.getProducts( function (err, response, data) {
-        res.status(200).json({
-            status: 'success',
-            data: data,
-        });
+        
+        if(data) {  
+            res.status(200).json({
+                status: 'success',
+                data: data,
+            });
+        } else {
+            res.status(418).json({
+                status: 'failed',
+                data: err
+            });
+        }
     });
 });
 
 app.get('/utils/currencies', function ( req, res ) {
     publicClient.getCurrencies( function ( err, response, data ) {
-        res.status(200).json({
-            status: 'success',
-            data: data,
-        });
+        if(data) {  
+            res.status(200).json({
+                status: 'success',
+                data: data,
+            });
+        } else {
+            res.status(418).json({
+                status: 'failed',
+                data: err
+            });
+        }
     });
 });
 
 app.get('/utils/getProduct24HrStats', function ( req, res) {
     publicClient.getProduct24HrStats( function ( err, response, data ) {
-        res.status(200).json({
-            status: 'success',
-            data: data,
-        });
+        if(data) {  
+            res.status(200).json({
+                status: 'success',
+                data: data,
+            });
+        } else {
+            res.status(418).json({
+                status: 'failed',
+                data: err
+            });
+        }
     });
 });
 
 app.get('/utils/getValue/:btc', function ( req, res) {
     publicClient.getProduct24HrStats( function ( err, response, data ) {
-        
-        var highPrice = parseFloat(data.high);
-        var lowPrice = parseFloat(data.low);
-        var input = parseFloat(req.params.btc);
-        
-        var avg = (highPrice + lowPrice) / 2;
 
-        var est = input* avg;
-        
-        res.status(200).json({
-            status: 'success',
-            estimatedValue: est,
-            data: data,
-        });
+        if(data) { 
+            //parse strings to float
+            var highPrice = parseFloat(data.high);
+            var lowPrice = parseFloat(data.low);
+            var latest = parseFloat(data.last);
+            var input = parseFloat(req.params.btc);
+
+            //calc averages/latest
+            var avgEst = valueAverage.getAverageValue(highPrice, lowPrice, input);
+            var latestEst = valueLatest.getLatestValue(latest, input);
+
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                averageEstimate: avgEst,
+                latestEstimate: latestEst
+            });
+        } else {
+            res.status(418).json({
+                status: 'failed',
+                data: err
+            });
+        }
     });
 });
 
